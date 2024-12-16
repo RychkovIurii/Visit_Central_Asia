d7edfc1c0c98bf3e75b9765f4d2e2f92ac9201a6
Iurii Rychkov <rychkoviurii@hotmail.com>
2024-12-16 18:38:02 +0200

    Added cors.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies or Authorization headers
+}));
+
 app.use(express.json());
 
 // Routes
