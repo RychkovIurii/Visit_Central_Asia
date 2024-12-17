6e4d52e15cce2b295a5b0c8f1f7a9d797539b644
Iurii Rychkov <rychkoviurii@hotmail.com>
2024-12-17 17:30:30 +0200

    Added cart to user.

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,15 @@ const UserSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	role: { type: String, enum: ['user', 'admin'], default: 'user' },
+	cart: [
+		{
+			tourId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tour' },
+			name: String,
+			price: Number,
+			startDate: Date,
+			endDate: Date
+		}
+	],
 });
 
 module.exports = mongoose.model('User', UserSchema);
