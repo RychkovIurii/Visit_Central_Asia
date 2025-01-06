c1a91908028de9c29fdeb9f644d8f556773fdffe
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-06 12:57:22 +0200

    Card Items images switched static to cloud.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,15 +2,21 @@ import React, { useContext } from 'react';
 import { LanguageContext } from "../context/LanguageContext";
 import "./CardStyles.css";
 import CardItems from './CardItems';
-import cardImage6 from '../assets/card6.jpg';
+/* import cardImage6 from '../assets/card6.jpg';
 import cardImage5 from '../assets/card5.jpg';
 import cardImage4 from '../assets/card4.jpg';
 import cardImage3 from '../assets/card3.jpg';
 import cardImage2 from '../assets/card2.jpg';
-import cardImage1 from '../assets/card1.jpg';
+import cardImage1 from '../assets/card1.jpg'; */
 
 const Card = () => {
 	const { t } = useContext(LanguageContext);
+	const cardImage6 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/turkestan1.jpg";
+	const cardImage5 = "https://res.cloudinary.com/dilugzsoa/image/upload/v1735299164/tours/kolsay1.jpg";
+	const cardImage4 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/furmanov1.jpg";
+	const cardImage3 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/kel-suu1.jpg";
+	const cardImage2 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/nomadgames1.jpg";
+	const cardImage1 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/grigoriev1.jpg";
 	return(
 		<div className="card">
 			<h1>{t("card.H1")}</h1>
