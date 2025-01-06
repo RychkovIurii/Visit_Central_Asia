acd18339c05df507c7efc77dad71820426d2ed33
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-06 18:28:12 +0200

    StayCard images switched static to cloud.

--- frontend/src/components/StayCard.jsx
+++ frontend/src/components/StayCard.jsx
@@ -2,15 +2,21 @@ import React, { useContext } from 'react';
 import { LanguageContext } from "../context/LanguageContext";
 import "./CardStyles.css";
 import CardItems from './CardItems';
-import cardImage6 from '../assets/stay6.jpeg';
+/* import cardImage6 from '../assets/stay6.jpeg';
 import cardImage5 from '../assets/stay5.jpeg';
 import cardImage4 from '../assets/stay4.jpeg';
 import cardImage3 from '../assets/stay3.jpeg';
 import cardImage2 from '../assets/stay2.jpeg';
-import cardImage1 from '../assets/stay1.jpeg';
+import cardImage1 from '../assets/stay1.jpeg'; */
 
 const StayCard = () => {
 	const { t } = useContext(LanguageContext);
+	const cardImage1 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/chunkurchak-resort.jpg";
+	const cardImage2 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/tenir-eco.jpg";
+	const cardImage3 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/beltam-yurt.jpg";
+	const cardImage4 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/hotel-uzbekistan.jpg";
+	const cardImage5 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/karven-four-seasons.jpg";
+	const cardImage6 = "https://res.cloudinary.com/dilugzsoa/image/upload/tours/hotel-kazakhstan.jpg";
 	return(
 		<div className="card">
 			<h1>{t("stay.H1")}</h1>
